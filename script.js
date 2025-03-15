111735c7b0a8be491a36db55afb06505694c7246
ExistoT01 <89727751+ExistoT01@users.noreply.github.com>
2025-03-15 11:11:28 +0800

    Update script.js
    add license

--- script.js
+++ script.js
@@ -7,6 +7,7 @@
 // @match        https://*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
+// @license MIT
 // ==/UserScript==
 
 (function() {
